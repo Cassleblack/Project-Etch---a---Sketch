cff91c00390a7c5b7becc42a37f88767318de7f6
olowe christian <olowechristian@gmail.com>
2024-10-05 23:14:10 +0100

    created an if statement to reset the square div and ask for the user input through a prompt

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function squareGrid (clickEvent) {
     const container = document.querySelector("#container");
-    let 
+    let numOfSquares = 32;
+
+    if (clickEvent) {
+        const squares = document.querySelectorAll(".square");
+        squares.forEach((square) => {
+            square.remove();
+        });
+
+        do {
+            numOfSquares = parseInt(prompt("Enter the numbeer of square you want: "));
+
+            if (numOfSquares > 100) alert("Enter a number between 1 & 100");
+        } while (numOfSquares > 100)
+    }
+
+    
 }
